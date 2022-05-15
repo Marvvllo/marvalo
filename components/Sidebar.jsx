b7a58c4dd5eee28475bb83e98ae3104ff0631411
Marvvllo <marvtfs@gmail.com>
2022-05-16 00:20:35 +0800

    Added active on role items

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -2,6 +2,7 @@ import SidebarItem from "./SidebarItem";
 import styles from "../styles/Sidebar.module.css";
 import Link from "next/link";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 // JSX Templates for mapping the agents
 const mapAgent = (agent) => <SidebarItem agent={agent} />;
@@ -12,6 +13,7 @@ const Sidebar = ({
   initiators,
   sentinels,
 }) => {
+  const { asPath } = useRouter();
   return (
     <div className={styles.container}>
       <Link href={"/"}>
@@ -24,53 +26,109 @@ const Sidebar = ({
         </a>
       </Link>
       <ul className={styles.list}>
-        <li>
-          <Link href={"/role/controller"}>
-            <a className={styles.item}>
-              <Image
-                src={require("../images/ic_controller.png")}
-                width={500}
-                height={500}
-              />
-            </a>
-          </Link>
-        </li>
+        {asPath == "/roles/controller" ? (
+          <li>
+            <Link href={"/roles/controller"}>
+              <a className={`${styles.item} ${styles.active}`}>
+                <Image
+                  src={require("../images/ic_controller.png")}
+                  width={500}
+                  height={500}
+                />
+              </a>
+            </Link>
+          </li>
+        ) : (
+          <li>
+            <Link href={"/roles/controller"}>
+              <a className={styles.item}>
+                <Image
+                  src={require("../images/ic_controller.png")}
+                  width={500}
+                  height={500}
+                />
+              </a>
+            </Link>
+          </li>
+        )}
         {controllers.map(mapAgent)}
-        <li>
-          <Link href={"/role/duelist"}>
-            <a className={styles.item}>
-              <Image
-                src={require("../images/ic_duelist.png")}
-                width={500}
-                height={500}
-              />
-            </a>
-          </Link>
-        </li>
+        {asPath == "/roles/duelist" ? (
+          <li>
+            <Link href={"/roles/duelist"}>
+              <a className={`${styles.item} ${styles.active}`}>
+                <Image
+                  src={require("../images/ic_duelist.png")}
+                  width={500}
+                  height={500}
+                />
+              </a>
+            </Link>
+          </li>
+        ) : (
+          <li>
+            <Link href={"/roles/duelist"}>
+              <a className={styles.item}>
+                <Image
+                  src={require("../images/ic_duelist.png")}
+                  width={500}
+                  height={500}
+                />
+              </a>
+            </Link>
+          </li>
+        )}
         {duelists.map(mapAgent)}
-        <li>
-          <Link href={"/role/initiator"}>
-            <a className={styles.item}>
-              <Image
-                src={require("../images/ic_initiator.png")}
-                width={500}
-                height={500}
-              />
-            </a>
-          </Link>
-        </li>
+        {asPath == "/roles/initiator" ? (
+          <li>
+            <Link href={"/roles/initiator"}>
+              <a className={`${styles.item} ${styles.active}`}>
+                <Image
+                  src={require("../images/ic_initiator.png")}
+                  width={500}
+                  height={500}
+                />
+              </a>
+            </Link>
+          </li>
+        ) : (
+          <li>
+            <Link href={"/roles/initiator"}>
+              <a className={styles.item}>
+                <Image
+                  src={require("../images/ic_initiator.png")}
+                  width={500}
+                  height={500}
+                />
+              </a>
+            </Link>
+          </li>
+        )}
         {initiators.map(mapAgent)}
-        <li>
-          <Link href={"/role/sentinel"}>
-            <a className={styles.item}>
-              <Image
-                src={require("../images/ic_sentinel.png")}
-                width={500}
-                height={500}
-              />
-            </a>
-          </Link>
-        </li>
+        {asPath == "/roles/sentinel" ? (
+          <li>
+            <Link href={"/roles/sentinel"}>
+              <a className={`${styles.item} ${styles.active}`}>
+                <Image
+                  src={require("../images/ic_duelist.png")}
+                  width={500}
+                  height={500}
+                />
+              </a>
+            </Link>
+          </li>
+        ) : (
+          <li>
+            <Link href={"/roles/sentinel"}>
+              <a className={styles.item}>
+                <Image
+                  src={require("../images/ic_sentinel.png")}
+                  width={500}
+                  height={500}
+                />
+              </a>
+            </Link>
+          </li>
+        )}
         {sentinels.map(mapAgent)}
       </ul>
     </div>
