2cc6abca4a04dd51ee4e8c12a207a91638bdff70
Marvvllo <marvtfs@gmail.com>
2022-10-14 08:19:47 +0800

    Migrate to Preact

--- next.config.js
+++ next.config.js
@@ -9,4 +9,19 @@ module.exports = {
     domains: ["picsum.photos", "media.valorant-api.com"],
     formats: ["image/avif", "image/webp"],
   },
+  webpack: (config, { dev, isServer }) => {
+
+    // Note, preact is only enabled for production builds (`next build`)
+    if (!dev && !isServer) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      };
+    }
+
+    return config;
+  },
 };
